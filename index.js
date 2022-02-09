b9c10c2da8252adb2edc50982fa7d8447f11d079
Stacksolution <yahooodemo@gmail.com>
2022-02-09 12:12:07 +0530

    chage index file

--- index.js
+++ index.js
@@ -1,37 +1,88 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http);
-
-
-app.get('/', (req, res) => {
-    res.send("Node Server is running. Yay!!")
-})
-
-io.on('connection', socket => {
-    //Get the chatID of the user and join in a room of the same chatID
-    chatID = socket.handshake.query.chatID
-    socket.join(chatID)
-
-    //Leave the room if the user closes the socket
-    socket.on('disconnect', () => {
-        socket.leave(chatID)
-    })
-
-    //Send message to only a particular user
-    socket.on('send_message', message => {
-        receiverChatID = message.receiverChatID
-        senderChatID = message.senderChatID
-        content = message.content
-
-        //Send message to only that particular room
-        socket.in(receiverChatID).emit('receive_message', {
-            'content': content,
-            'senderChatID': senderChatID,
-            'receiverChatID': receiverChatID,
-        })
-    })
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+// Reserved Events
+let ON_CONNECTION = 'connection';
+let ON_DISCONNECT = 'disconnect';
+// Main Events
+let EVENT_IS_USER_ONLINE = 'check_online';
+let EVENT_SINGLE_CHAT_MESSAGE = 'single_chat_message';
+// Sub Events
+let SUB_EVENT_RECEIVE_MESSAGE = 'receive_message';
+let SUB_EVENT_MESSAGE_FROM_SERVER = 'message_from_server';
+let SUB_EVENT_IS_USER_CONNECTED = 'is_user_connected';
+let listen_port = 4002;
+// Status
+let STATUS_MESSAGE_NOT_SENT = 10001;
+let STATUS_MESSAGE_SENT = 10002;
+// This map has all users connected
+const userMap = new Map();
+io.sockets.on(ON_CONNECTION, function (socket) {
+    onEachUserConnection(socket);
 });
+// This function is fired when each user connects to socket
+function onEachUserConnection(socket) {
+    print('---------------------------------------');
+    print('Connected => Socket ID ' + socket.id + ', User: ' + JSON.stringify(socket.handshake.query));
+    var from_user_id = socket.handshake.query.from;
+    // Add to Map
+    let userMapVal = { socket_id: socket.id };
+    addUserToMap(from_user_id, userMapVal);
+    print(userMap);
+    printNumOnlineUsers();
+    onMessage(socket);
+    checkOnline(socket);
+    onUserDisconnect(socket);
+}
+function addUserToMap(key_user_id, val) {
+    userMap.set(key_user_id, val);
+}
+function removeUserWithSocketIdFromMap(socket_id) {
+    print('Deleting user with socket id: ' + socket_id);
+    let toDeleteUser;
+    for (let key of userMap) {
+        // index 1, returns the value for each map key
+        let userMapValue = key[1];
+        if (userMapValue.socket_id == socket_id) {
+            toDeleteUser = key[0];
+        }
+    }
+    print('Deleting User: ' + toDeleteUser);
+    if (undefined != toDeleteUser) {
+        userMap.delete(toDeleteUser);
+    }
+    print(userMap);
+    printNumOnlineUsers();
+}
+function getSocketIDfromMapForthisUser(to_user_id) {
+    let userMapVal = userMap.get(`${to_user_id}`);
+    if (userMapVal == undefined) {
+        return undefined;
+    }
+    return userMapVal.socket_id;
+}
+function sendBackToClient(socket, event, message) {
+    socket.emit(event, stringifyJson(message));
+}
+function sendToConnectedSocket(socket, to_user_socket_id, event, message) {
+    socket.to(`${to_user_socket_id}`).emit(event, stringifyJson(message));
+}
+function userFoundOnMap(to_user_id) {
+    let to_user_socket_id = getSocketIDfromMapForthisUser(to_user_id);
+    return to_user_socket_id != undefined;
+}
+// Always stringify to create proper json before sending.
+function stringifyJson(data) {
+    return JSON.stringify(data);
+}
+function print(logData) {
+    console.log(logData);
+}
+function printNumOnlineUsers() {
+    print('Online Users: ' + userMap.size);
+}
 
-http.listen(process.env.PORT || 3000, function () {
-    console.log('Start the server in Port *: ' + process.env.PORT);
+server.listen(listen_port || 3000, function () {
+    console.log('Start the server in Port *: ' + listen_port);
 });
